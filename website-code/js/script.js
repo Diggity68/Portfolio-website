ddea347b350421e16c743e7a9d1056e954692343
Harden Phillip <34043808+Phillip-Harden@users.noreply.github.com>
2018-04-04 15:28:57 -0700

    Slideshow at leasts shows slides

--- website-code/js/script.js
+++ website-code/js/script.js
@@ -1,9 +1,11 @@
+console.log("working");
 var myCards = 1;
-showSlide(myCards);
 
+//The controls to make slide go forward or back
 function plusSlides(n) {
     showSlide(myCards += n);
 }
+
 
 function currentSlide(n) {
     showSlide(myCards = n);
@@ -13,11 +15,25 @@ function showSlide(n) {
     var i;
     var slides = document.getElementsByClassName("card");
     var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {myCards = 1}
-    if (n < 1) {myCards = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        dots[i].className.replace (" active", "");
+    var slideLink = document.getElementsByClassName("link")
+    if (n > slides.length) {
+        myCards = 1
     }
+    if (n < 1) {
+        myCards = slides.length
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className.replace(" active", "");
+    }
+//    for (i = 0; i < slideLink.length; i++) {
+//        links[i].style.display = "none";
+//    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+
     slides[myCards - 1].style.display = "block";
     dots[myCards - 1].className += " active";
+
 }
